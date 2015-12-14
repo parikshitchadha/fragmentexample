6ac743450257910b0badfc1d2253ec644971b87d
parikshit <parikshitchadha@gmail.com>
2015-12-14 17:59:56 +0530

    just added a comment

--- app/src/main/java/com/example/parikshit/fragmentexample/MainActivity.java
+++ app/src/main/java/com/example/parikshit/fragmentexample/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
+//        hi this is parikshit chadha
+//                and i love this job
     }
 }

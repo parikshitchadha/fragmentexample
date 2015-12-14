50fae6ab286fa508c5c93fab1480e44a477b922d
parikshit <parikshitchadha@gmail.com>
2015-12-14 17:36:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/parikshit/fragmentexample/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.parikshit.fragmentexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+    }
+}
